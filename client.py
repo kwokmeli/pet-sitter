c56607076baa139a32be559d402be7ef165ab560
mlkwok <mkwok@uw.edu>
2017-05-14 14:27:26 -0700

    fixed socket re-binding (?)

--- client.py
+++ client.py
@@ -4,10 +4,19 @@ import sys
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('192.168.0.67', 9999))
 
-for args in sys.argv:
-    if args == "":
-        args = 'no args'
-    else:
-        s.send(args + ' ')
+try:
+    message = raw_input('Enter your message: ')
+    print >>sys.stderr, 'sending "%s"' % message
+    s.sendall(message)
 
-print 'goodbye!'
+    amount_received = 0
+    amount_expected = len(message)
+
+    while amount_received < amount_expected:
+        data = s.recv(16)
+        amount_received += len(data)
+        print >>sys.stderr, 'received "%s"' % data
+
+finally:
+    print >>sys.stderr, 'closing socket'
+    s.close() 
